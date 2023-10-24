f6e4e4b4c42ff564a47c20bd80adecfe325b5dc1
peam1 <peam1@PEEMNA>
2023-10-24 11:53:19 +0700

    Add project files.

---
+++ GradingSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GradingSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter score: ");
+            int score;
+            string input =Console.ReadLine();
+
+
+            if (int.TryParse(input, out score))
+            {
+                Grading.GradeCalculator(score);
+            }
+            else
+            {
+                Console.WriteLine("Error: Score Score should be in digit only(" + input + ")");
+            }
+        }
+    }
+}
